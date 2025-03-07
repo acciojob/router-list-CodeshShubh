17c5213633c3207fb511aea9aedaa322160adbf5
CodeshShubh <codeshshubh@gmail.com>
2025-03-07 21:26:53 +0530

    completed

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 
 const items = [
-    { id: 1, name: "Item 1", description: "Description of Item 1." },
-    { id: 2, name: "Item 2", description: "Description of Item 2." },
-    { id: 3, name: "Item 3", description: "Description of Item 3." },
+    { id: 1, name: "Item 1", description: "Description for Item 1" }, 
+    { id: 2, name: "Item 2", description: "Description for Item 2" },
+    { id: 3, name: "Item 3", description: "Description for Item 3" },
   ];
 
 const ItemList = () => {
   return (
     <div>
-        <h1>Items List</h1>
+        <h1>Item List</h1> 
         <ul>
            {
             items.map((item)=>{
